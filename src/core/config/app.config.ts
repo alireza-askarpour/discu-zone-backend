30a89e35d8034d474a7153e1c003cdca77690691
Alireza Askarpour <askarpourdev@gmail.com>
2023-07-27 16:28:30 -0400

    refactor: config environment variables

--- src/core/config/app.config.ts
+++ src/core/config/app.config.ts
@@ -1,10 +1,43 @@
+import { ConfigService } from '@nestjs/config';
+import * as process from 'process';
 import { bold } from 'chalk';
 
-export const isDevelopment = process.env.NODE_ENV === 'development';
-export const isProduction = process.env.NODE_ENV === 'production';
+export interface Configs {
+  PORT: number;
+  NODE_ENV: string;
+  DB_HOST: string;
+  DB_PORT: string;
+  DB_USER: string;
+  DB_PASS: string;
+  DB_DIALECT: string;
+  DB_NAME_TEST: string;
+  DB_NAME_DEVELOPMENT: string;
+  DB_NAME_PRODUCTION: string;
+  JWTKEY: string;
+  TOKEN_EXPIRATION: string;
+  BEARER: string;
+}
 
-export const port = process.env.PORT || 8000;
-export const mode = process.env.NODE_ENV || 'development';
+export default (): Configs => ({
+  PORT: Number(process.env.PORT),
+  NODE_ENV: process.env.NODE_ENV,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_DIALECT: process.env.DB_DIALECT,
+  DB_NAME_TEST: process.env.DB_NAME_TEST,
+  DB_NAME_DEVELOPMENT: process.env.DB_NAME_DEVELOPMENT,
+  DB_NAME_PRODUCTION: process.env.DB_NAME_PRODUCTION,
+  JWTKEY: process.env.JWTKEY,
+  TOKEN_EXPIRATION: process.env.TOKEN_EXPIRATION,
+  BEARER: process.env.BEARER,
+});
+
+const configService: ConfigService<Configs> = new ConfigService<Configs>();
+
+export const port = configService.get('PORT') || 3000;
+export const mode = configService.get('NODE_ENV') || 'development';
 
 export const appListener = () => {
   const runningMode = `Server running in ${bold(mode)} mode`;
