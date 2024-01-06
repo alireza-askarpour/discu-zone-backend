5b5da00c7aff746ba0fe556c0a142bc8f43eb01f
Alireza Asakrpour <askarpourdev@gmail.com>
2024-01-06 15:01:17 -0500

    refactor: update get me with new changes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,10 @@
-import { Request } from 'express';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Controller, UnauthorizedException, Req } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { GetMeDecorator } from './decorators/get-me.decorator';
 import { ResponseMessages } from 'src/common/constants/response-messages.constant';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @ApiBearerAuth()
 @ApiTags('Users')
@@ -13,10 +13,10 @@ export class UsersController {
   constructor(private userService: UsersService) {}
 
   @GetMeDecorator()
-  async getMe(@Req() req: Request) {
-    // if (!req.user) {
-    //   throw new UnauthorizedException(ResponseMessages.UNAUTHORIZED);
-    // }
-    // return this.userService.getMe(req.user.id);
+  async getMe(@CurrentUser() userId: string) {
+    if (!userId) {
+      throw new UnauthorizedException(ResponseMessages.UNAUTHORIZED);
+    }
+    return this.userService.getMe(userId);
   }
 }
